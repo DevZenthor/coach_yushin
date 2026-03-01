8d75247d2cfa60c6627f5e40f559790a1f55132a
DevZenthor <zenthorpro@outlook.com>
2026-03-01 14:39:46 +0100

    add: picture et gif

--- Coach_Yushin/src/main.js
+++ Coach_Yushin/src/main.js
@@ -1,24 +1,58 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const app = document.querySelector("#app");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+app.innerHTML = `
+<header>
+  <h1>Coach Yushin</h1>
+  <nav>
+    <a href="#hero">Accueil</a>
+    <a href="#reseaux">Réseaux</a>
+    <a href="#experience">Expérience</a>
+    <a href="#collaboration">Collaborations</a>
+  </nav>
+</header>
+
+<!-- HERO / Présentation -->
+<section id="hero">
+  <div class="hero-container">
+    <img src="/yushin.jpg" alt="Coach Yushin" class="avatar">
+    <h2>Coach Yushin</h2>
+    <p class="description">Fortnite Coach</p>
   </div>
-`
+</section>
 
-setupCounter(document.querySelector('#counter'))
+<section id="reseaux">
+  <ul>
+    <li><a href="https://twitter.com/coach" target="_blank">Twitter</a></li>
+    <li><a href="https://twitch.tv/coach" target="_blank">Twitch</a></li>
+    <li><a href="https://discord.gg/coach" target="_blank">Discord</a></li>
+  </ul>
+</section>
+
+<section id="experience">
+  <h2>Expérience</h2>
+  <ul>
+    <li>Coach depuis 2022</li>
+    <li>+200 joueurs coachés</li>
+    <li>Analyse VOD</li>
+    <li>Stratégies compétitives</li>
+  </ul>
+</section>
+
+<section id="collaboration">
+  <h2>Collaborations actuelles</h2>
+  <ul>
+    <li>Equipe Esport XYZ</li>
+    <li>Créateurs Fortnite</li>
+    <li>Tournois privés</li>
+  </ul>
+</section>
+
+<footer>
+  <p>Coach Yushin</p>
+  <p>
+    <a href="https://twitter.com/coach" target="_blank">Twitter</a> |
+    <a href="https://twitch.tv/coach" target="_blank">Twitch</a> |
+    <a href="https://discord.gg/coach" target="_blank">Discord</a>
+  </p>
+</footer>
+`;
